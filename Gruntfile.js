3bbbebafd466cc0e79607768bb62b9f1408b3059
Stephan Bönnemann <stephan@boennemann.me>
2014-07-18 21:27:16 +0200

    chore(gh-pages): setup CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,6 +249,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
+            'CNAME',
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
