aeca5901d4081b58aff2cc1e0f443735d66e523e
Stephan Bönnemann <stephan@boennemann.me>
2015-07-01 16:44:03 +0200

    test: fix setup

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
